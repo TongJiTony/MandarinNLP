df3199258411aa5e7739942dc6ecc8643d19f8ba
zixun huang <2153679@mail.tongji.edu.cn>
2025-04-03 13:00:13 +0800

    add DIYCRFSegmenter and pass test cases

--- test.py
+++ test.py
@@ -50,12 +50,13 @@ if __name__ == "__main__":
     # 加载数据
     test_lines, gold_lines = load_data(test_file, gold_file)
 
-    # 初始化分词器(CRF需要进行训练， 机械分词可使用FMM或RMinM，读取dict字典)
-    # segmenter = FMMSegmenter('dict.txt')
-    segmenter = DIYCRFSegmenter()
-    # 训练模型
-    train_file = "bmes_train_pku.txt"
-    segmenter.train(train_file)
+    # 初始化分词器(通过注释不同段落进行选择，CRF需要进行训练， 机械分词可使用FMM或RMinM，读取dict字典)
+    segmenter = FMMSegmenter('dict.txt')
+
+    # # 训练模型
+    # segmenter = DIYCRFSegmenter()
+    # train_file = "bmes_train_pku.txt"
+    # segmenter.train(train_file)
 
     # 测试分词器
     y_true, y_pred = test_segmenter(segmenter, test_lines, gold_lines)
